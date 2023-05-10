41c3bcaacc3fd4ab71d9bcc5f708ff27abd90f10
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-05-10 09:51:55 +0000

    tasks

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""A module for using the Redis NoSQL data storage"""
+import uuid
+import redis
+from functools import wraps
+from typing import Any, Callable, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    """Tracks the number of calls made to a method in a Cache class"""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Invokes the given method after incrementing its call counter"""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+
+def call_history(method: Callable) -> Callable:
+    """Tracks the call details of a method in a Cache class"""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Returns the method's output after storing its input and output"""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}.outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
